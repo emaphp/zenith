76945f6a737464ee96f927ad3587eccec78037b9
emaphp <emmanuel.antico@gmail.com>
2013-08-01 02:30:14 -0300

    Updated: Request example for Acme/Helloworld service.

--- app/components/Acme/HelloWorld.php
+++ app/components/Acme/HelloWorld.php
@@ -12,6 +12,20 @@ class HelloWorld extends Service {
 	 * @param Request $request
 	 * @param Response $response
 	 * @return string
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">hello</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *			   <parameter xsi:type="urn:Parameter"/>
+	 *		   </urn:execute>
+	 *    </soapenv:Body>
+	 * </soapenv:Envelope>
 	 */
 	public function hello(Request $request, Response $response) {
 		return 'Hello World :)';
@@ -21,6 +35,20 @@ class HelloWorld extends Service {
 	 * Renders a template through the 'view' property
 	 * @param Request $request
 	 * @param Response $response
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">sayHi</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *             <parameter xsi:type="urn:Parameter"/>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
 	 */
 	public function sayHi(Request $request, Response $response) {
 		return $this->view->render('Acme/hello_world', array('message' => 'Hello World!!!', 'destination' => 'Earth'));
@@ -30,6 +58,25 @@ class HelloWorld extends Service {
 	 * Obtains a configuration value from Request
 	 * @param Request $request
 	 * @param Response $response
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">sayGoodbye</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration">
+	 *                 <option xsi:type="urn:Option">
+	 *                     <name xsi:type="xsd:string">lang</name>
+	 *                     <value xsi:type="xsd:string">sp</value>
+	 *                 </option>
+	 *             </configuration>
+	 *             <parameter xsi:type="urn:Parameter"/>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
 	 */
 	public function sayGoodbye(Request $request, Response $response) {
 		//obtain option 'lang'
@@ -41,7 +88,7 @@ class HelloWorld extends Service {
 		}
 		elseif (!empty($lang) && $lang != 'en') {
 			//log notice
-			Zenith\Application::getInstance()->logger->addNotice("Unrecognized language '$lang'");
+			\Zenith\Application::getInstance()->logger->addNotice("Unrecognized language '$lang'");
 		}
 		
 		return $this->view->render('Acme/hello_world', $args);
@@ -49,6 +96,22 @@ class HelloWorld extends Service {
 	
 	/**
 	 * Obtains class public methods through the Acme/ReflectionComponent class
+	 * @param Request $request
+	 * @param Response $response
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">expose</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *             <parameter xsi:type="urn:Parameter"/>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
 	 */
 	public function expose(Request $request, Response $response) {
 		$component = new Reflection\ReflectionComponent();
@@ -60,6 +123,25 @@ class HelloWorld extends Service {
 	 * Parses request to a simplexml and DOMDocument objects
 	 * @param Request $request
 	 * @param Response $response
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">parseRequest</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *             <parameter xsi:type="urn:Parameter">
+	 *                 <user>
+	 *                     <id>1234</id>
+	 *                     <name>jdoe</name>
+	 *                 </user>
+	 *             </parameter>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
 	 */
 	public function parseRequest(Request $request, Response $response) {
 		$xml = $request->getParameter(Request::AS_SIMPLEXML);
@@ -73,14 +155,74 @@ class HelloWorld extends Service {
 		$response->setResult($this->view->render('Acme/user', array('user_id' => $user_id, 'user_name' => $name->nodeValue)));
 	}
 	
+	/**
+	 * Throws a SOAP Fault
+	 * @param Request $request
+	 * @param Response $response
+	 * @throws \SoapFault
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">throw_fault</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *             <parameter xsi:type="urn:Parameter"/>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
+	 */
 	public function throw_fault(Request $request, Response $response) {
 		throw new \SoapFault("Server", "Unexpected error");
 	}
 	
+	/**
+	 * Throws an exception
+	 * @param Request $request
+	 * @param Response $response
+	 * @throws \Exception
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">throw_exception</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *             <parameter xsi:type="urn:Parameter"/>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
+	 */
 	public function throw_exception(Request $request, Response $response) {
 		throw new \Exception("Something bad happened...");
 	}
 	
+	/**
+	 * Throws a custom service exception
+	 * @param Request $request
+	 * @param Response $response
+	 * @throws ServiceException
+	 * 
+	 * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ZenithService">
+	 *     <soapenv:Header/>
+	 *     <soapenv:Body>
+	 *         <urn:execute soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+	 *             <service xsi:type="urn:Service">
+	 *                 <class xsi:type="xsd:string">Acme/HelloWorld</class>
+	 *                 <method xsi:type="xsd:string">throw_service_exception</method>
+	 *             </service>
+	 *             <configuration xsi:type="urn:Configuration"/>
+	 *             <parameter xsi:type="urn:Parameter"/>
+	 *         </urn:execute>
+	 *     </soapenv:Body>
+	 * </soapenv:Envelope>
+	 */
 	public function throw_service_exception(Request $request, Response $response) {
 		throw new ServiceException(5, "A customized error response");
 	}
