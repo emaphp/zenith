4e2a290e69ab2db6e6710abdb01c8bcde3590802
emaphp <emmanuel.antico@gmail.com>
2014-07-07 22:43:17 -0300

    Fixed: Added components path in paths.php

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -3,13 +3,14 @@
  * Zenith application directories
  */
 return [
-		'current'  => getcwd(),            // current working directory
-		'app'      => 'app/',              // application directory
-		'config'   => 'app/config/',       //configuration directory
-		'services' => 'app/services/',     // services directory
-		'views'    => 'app/views/',        // views directory
-		'storage'  => 'app/storage/',      // application storage directory
-		'wsdl'     => 'app/storage/wsdl/', // wdsl directory
-		'logs'     => 'app/storage/logs/', // logs directory
-		'twig'     => 'app/storage/twig'   // cached templates directory
+		'current'    => getcwd(),            // current working directory
+		'app'        => 'app/',              // application directory
+		'config'     => 'app/config/',       // configuration directory
+		'services'   => 'app/services/',     // services directory
+		'components' => 'app/components/',   // components directory
+		'views'      => 'app/views/',        // views directory
+		'storage'    => 'app/storage/',      // application storage directory
+		'wsdl'       => 'app/storage/wsdl/', // wdsl directory
+		'logs'       => 'app/storage/logs/', // logs directory
+		'twig'       => 'app/storage/twig'   // cached templates directory
 ];
